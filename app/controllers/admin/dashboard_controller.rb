a497c0a8c5d4ccb8cf745505ea8934f5025abad5
habibcodes <habib.codes@gmail.com>
2021-11-10 20:24:33 -0500

    add dashboard and dynamically updated counter for total products and total product categories in the DB; basic CSS BS3 styling applied

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"].to_s, password: ENV["ADMIN_PASSWORD"].to_s
 
   def show
+    # return total number of both products and categories of items
+    @total_product_count = Product.count
+    @total_category_count = Category.count
   end
 end
