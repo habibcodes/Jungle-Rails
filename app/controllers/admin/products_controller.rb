f7a5c899c28cc4b212a8976d1d9bee5a4eacc8c7
habibcodes <habib.codes@gmail.com>
2021-11-10 19:47:00 -0500

    add basic authentication route to admin pages

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  # basic HTTP auth for admin to restrict products access here
+  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"].to_s, password: ENV["ADMIN_PASSWORD"].to_s
 
   def index
     @products = Product.order(id: :desc).all
